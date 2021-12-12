10bf1e5c1e470d8db11f79b546e99303f82b2f5c
buckmelanoma <43704682+buckmelanoma@users.noreply.github.com>
2021-12-12 08:29:03 -0800

    Initial commit

---
+++ libreddit_redirect.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name               Libredd.it redirect
+// @namespace          https://greasyfork.org/en/users/6969696969
+// @description        Redirect links to libredd.it for privacy
+// @include            *://www.reddit.com/*
+// @exclude            *://www.reddit.com/poll/*
+// @version            1.02
+// @run-at             document-start
+// @author             uint2048_t
+// @grant              none
+// @icon               https://raw.githubusercontent.com/spikecodes/libreddit/master/static/favicon.ico
+// ==/UserScript==
+
+window.location.replace("https://libreddit.domain.glass" + window.location.pathname + window.location.search);
